45bbbdd895dadae1b8702d5047593fc46a68a8ac
Palina Bril <venera_48@mail.ru>
2023-06-30 01:49:49 +0300

    project

---
+++ src/main/java/by/aws/projectuno/ProjectUnoApplication.java
@@ -0,0 +1,13 @@
+package by.aws.projectuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectUnoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/aws/projectuno/ProjectUnoApplicationTests.java
@@ -0,0 +1,13 @@
+package by.aws.projectuno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectUnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
